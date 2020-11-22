8ff71e711f89a67facd742080fed3ac5b56bcf3f
sumanth rangaraju <sumanth rangaraju>
2020-11-22 16:43:20 -0600

    Added notification service

---
+++ src/main/java/com/santg/springboot/thymeleafdemo/config/AWSSNSConfig.java
@@ -0,0 +1,26 @@
+package com.santg.springboot.thymeleafdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+
+@Configuration
+public class AWSSNSConfig {
+	public static final String SECRET_KEY = "rD65S1ZnRRE7QwXHAe1XxE1rLdvNEhdNowYXJ5GS";
+    public static final String ACCESS_KEY = "AKIAI35SHPKPJJUPMFIQ";
+
+    @Primary
+    @Bean
+    public AmazonSNSClient getSnsClient() {
+        return (AmazonSNSClient) AmazonSNSClientBuilder.standard().withRegion(Regions.US_EAST_1)
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ACCESS_KEY,
+                        SECRET_KEY)))
+                .build();
+    }
+}
--- src/main/java/com/santg/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/santg/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -3,17 +3,26 @@ package com.santg.springboot.thymeleafdemo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.SubscribeRequest;
 import com.santg.springboot.thymeleafdemo.entity.Employee;
 import com.santg.springboot.thymeleafdemo.service.EmployeeService;
 
+@SpringBootApplication (exclude = {ContextStackAutoConfiguration.class, ContextRegionProviderAutoConfiguration.class})
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
@@ -24,6 +33,41 @@ public class EmployeeController {
 	public EmployeeController(EmployeeService employeeService) {
 		this.employeeService = employeeService;
 	}
+	
+	 @Autowired
+	    private AmazonSNSClient snsClient;
+
+	    String TOPIC_ARN= "arn:aws:sns:us-east-1:705258768232:springboot-crud-mysql" ;
+
+		@GetMapping("/addSubscription/{email}")
+		public String addSubscription(@PathVariable String email, RedirectAttributes redirectAttributes) {
+			SubscribeRequest request = new SubscribeRequest(TOPIC_ARN, "email", email);
+			snsClient.subscribe(request);
+			redirectAttributes.addFlashAttribute("message", "You have Successfully subscribed. To confirm the subscription, please check your email : "+email);
+			return "redirect:/employees/list";
+		}
+
+		 @GetMapping("/sendNotification")
+		public String publishMessageToTopic(RedirectAttributes redirectAttributes){
+			 PublishRequest publishRequest=new PublishRequest(TOPIC_ARN,buildEmailBody(),"Notification: Its holiday Time");
+			 snsClient.publish(publishRequest);
+			 redirectAttributes.addFlashAttribute("notification", "Users have received a notification. Please check your emails");
+			 return "redirect:/employees/list";
+		}
+
+
+
+		private String buildEmailBody(){
+			return "Hello Friend,\n" +
+					"\n" +
+					"Its holiday Time"+"\n"+
+					"Happy Thanksgiving and christmas \n" +
+					"\n" +
+					"\n" +
+					"Please check the following link for thanksgiving and blackfriday deals:" +
+					"\n" +
+					"https://www.businessinsider.com/black-friday-deals";
+		}
 	
 	// add mapping for "/list"
 	@GetMapping("/list")
